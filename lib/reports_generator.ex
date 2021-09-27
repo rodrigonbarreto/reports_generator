567c3333cc245c3444b31b024a3dcf6b85b28b96
Rodrig Barreto <rodrigonbarreto@gmail.com>
2021-09-27 16:05:26 +0200

    parsing and  Extracting for new module

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,8 +1,17 @@
 defmodule ReportsGenerator do
   def build(filename) do
-    case File.read("reports/#{filename}") do
-        {:ok, result } -> result
-        {:error, reason } -> reason
-    end
+    #opening a file with case.
+    # case File.read("reports/#{filename}") do
+    #     {:ok, result } -> result
+    #     {:error, reason } -> reason
+    # end
+    filename
+    |> ReportsGenerator.Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line , report -> sum_values(line, report) end)
+    # |> File.read
+    # |> handle_file()
   end
+
+  defp sum_values([id, _foodname, price], report), do: Map.put(report, id, report[id] + price)
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
